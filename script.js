1a48f0c7e75ab6ff11ee4417711dc975a3228ff1
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2025-12-19 14:05:53 +0000

    add basic BoxGeometry object to scene

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ import * as THREE from "./three.module.js"
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera()
+camera.position.z = 3
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshBasicMaterial()
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
 
 renderer.render(scene, camera)
