cec88323c9a5de93accbbbf1a4c490c368ca70a5
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2025-12-19 14:21:15 +0000

    separate function for resize event listener

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
-import * as THREE from "./three.module.js"
+import * as THREE from "./three.module.js";
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 camera.position.set(1, 1, 1);
 camera.lookAt(0, 0, 0);
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-document.body.appendChild(renderer.domElement)
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial()
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial();
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 function animate() {
     requestAnimationFrame(animate);
@@ -29,10 +23,12 @@ function animate() {
 }
 animate();
 
-window.addEventListener("resize", () => {
+function resize() {
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
     camera.aspect = windowWidth / windowHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(windowWidth, windowHeight);
-});
+}
+
+window.addEventListener("resize", resize);
