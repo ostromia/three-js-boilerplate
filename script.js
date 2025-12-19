63213bfb67cbd79961310db52bcf54c24199e4be
Berkay Yalin <66202981+ostromia@users.noreply.github.com>
2025-12-19 14:03:07 +0000

    initialise scene & camera

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 import * as THREE from "./three.module.js"
 
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera()
+
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+
+renderer.render(scene, camera)
